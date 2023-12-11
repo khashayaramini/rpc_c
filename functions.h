9fdb180a8d4d9ca425d56eac9885a168c294b608
khashayar <khashayar@whiteraven.one>
2023-12-11 12:01:14 +0300

    my_sub

--- functions.h
+++ functions.h
@@ -17,13 +17,22 @@ void my_add(void **args, void *res){
     *(int *)res = result;
 }
 
+void my_sub(void **args, void *res){
+    int i1 = *reinterpret_cast<int*>(args[0]);
+    int i2 = *reinterpret_cast<int*>(args[1]);
+    int result = i1 - i2;
+    std::cout << "answer: " << result << "\n";
+    *(int *)res = result;
+}
+
 struct {
     void (*fp)(void **args, void *res);
     const char * name;
 } func_array[] = {
     FUNC_DEF(my_add)
+    FUNC_DEF(my_sub)
 };
 
-int func_array_size = 1;
+int func_array_size = 2;
 
 #endif // FUNCTIONS_H
